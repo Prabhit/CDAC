44f3482ca986a85adc7d2a57e662f9b82029d194
Prabhit <112605929+Prabhit@users.noreply.github.com>
2024-08-22 15:35:50 +0530

    Add files via upload

---
+++ Logic_Building/Assignments/Assignment-1/Assignment-5.java
@@ -0,0 +1,14 @@
+
+
+public class Square {
+
+    public static void main(String[] args) {
+        
+        int number = 6;
+	int square = number * number;
+	System.out.println("Square is:" + square);
+        
+        
+        
+    }
+}
